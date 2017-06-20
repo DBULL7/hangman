2c8535a4ce11fadd8ab3fc1659da745bd82f807f
Devon Bull <dbull@live.com>
2017-06-20 00:48:06 -0500

    game ends on when chances equal zero

--- hangman.py
+++ hangman.py
@@ -2,12 +2,11 @@ import os
 import random
 import getpass
 chances = 5
-answer = ''
-guessedAnswer = ''
+
 os.system('clear')
 
 print('Welcome to Hangman')
-
+print()
 answer = getpass.getpass('Enter a Word:')
 guessedAnswer = '_'*len(answer)
 
@@ -15,6 +14,7 @@ guessedAnswer = '_'*len(answer)
 def makeGuess(answer):
     global guessedAnswer
     checkForWin(answer, guessedAnswer)
+    checkGameOver()
     print()
     print('Make a guess')
     print()
@@ -55,5 +55,15 @@ def checkForWin(answer, guessedAnswer):
         print('YOU WIN!')
         print()
         exit()
+
+
+def checkGameOver():
+    global chances
+    if chances == 0:
+        print()
+        print('GAMEOVER. YOU DIED.')
+        print()
+        exit()
+
 
 makeGuess(answer)
